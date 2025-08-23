ec452709004645f330a24022b8101c109b1e9496
Vandana <vivandana3@gmail.com>
2025-08-23 14:38:42 +0530

    day7

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"], 
   theme: {
+    screens:{
+      sm : "340px",
+      md:"540px",
+      lg:"768px",
+      xl:"1180px",
+    },
     extend: {},
+    container:{
+      center:true,
+      padding:{
+        DEFAULT: "12px",
+        md:"32px",
+      }
+    }
   },
   plugins: [],
 }
